ebd309fbaa54ce520338730997e9cbd1ad6948bf
Anastasia <anastasiastarodub@gmail.com>
2023-03-23 17:56:08 +0300

    Решение задачи 27

---
+++ Zadacha27/Program.cs
@@ -0,0 +1,30 @@
+﻿// Программа, которая принимает на вход число и выдает сумму цифр в числе
+
+Console.WriteLine("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+int Len(int A)
+{
+    int i = 0;
+    while (A > 0)
+    {
+        A = (A / 10);
+        i = i + 1;
+    }
+    return i;
+}
+int len = Len(A);
+
+int Sum(int A, int len)
+{
+    int result = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        result = result + (A % 10);
+        A = A / 10;
+
+    }
+    return result;
+}
+
+Console.WriteLine($"Сумма цифр в числе {A} равна {Sum(A, len)}");
