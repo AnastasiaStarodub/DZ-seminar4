6f758853ebc59fa9f9ec1ce3e99b8623b0ffddcb
Anastasia <anastasiastarodub@gmail.com>
2023-03-23 17:56:47 +0300

    Решение задачи 29

---
+++ Zadacha29/Program.cs
@@ -0,0 +1,29 @@
+﻿// Программу, которая задаёт массив из 8 элементов и выводит их на экран
+
+int[] Massive8()
+{
+    int n = 8;
+    int[] arr = new int[n];
+    Random rand = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next();
+    }
+    return arr; 
+} 
+
+int[] array = Massive8();
+
+void PrintArray(int [] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+PrintArray(array);
+
+
